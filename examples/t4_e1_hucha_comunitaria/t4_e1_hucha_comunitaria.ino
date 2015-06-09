f2af3668fc2d54ea857391a32b0d2479cdb15271
timearchitect <alrik_he@hotmail.com>
2015-06-09 19:31:16 +0200

    Transladed Piggybank example 4_1
    huncha comunitaria translated 4_1

--- examples/t4_e1_hucha_comunitaria/t4_e1_hucha_comunitaria.ino
+++ examples/t4_e1_hucha_comunitaria/t4_e1_hucha_comunitaria.ino
@@ -1,24 +1,53 @@
+/*
+ * Campamento de Verano 2015
+ *
+ * Tutorial 4 - Ejemplo 1 - HUCHA COMUNITARIA
+ *
+ * Conecta el LED rojo en el pin 00, el amarillo en el pin 01 y el verde en el pin 02
+ * Conecta los botones a los conectores I0, I1 e I2
+ *
+ * Cuando introduces la moneda y aprietas el boton correspondiente
+ * Este programa pone un tweet con la cantidad total de dinero
+ * Arduino cuenta la cantidad total de dinero que tu tienes y se pone a 0 cuando lo apagas
+ *
+ *
+ * Etiqueta tus botones para identificar diferentes tipos de monedas
+ * Ajusta los numeros en el codigo de abajo
+ *
+ * (c) 2015 D. Cuartielles para Arduino Verkstad
+ *
+ */
+
+
 #include <ZGZ_IoT.h>
 #include <Bridge.h>
 #include <HttpClient.h>
-#define COIN_VALUE_1 1
-#define COIN_VALUE_2 2
-#define COIN_VALUE_3 10
-
 
 HttpClient client;
+
+// cambia los valores basándote en las monedas que vas a utilizar
+const int VALOR_ROJO = 1;       // ajusta el valor de la moneda para e color rojo
+const int VALOR_AMARILLO = 2;    // ajusta el valor de la moneda para e color amarillo
+const int VALOR_VERDE = 10;  // ajusta el valor de la moneda para e color verde
+
+
+// Estado de los botones
 int boton1 = 0;
 int boton2 = 0;
 int boton3 = 0;
+
+// Inicializa los botones para detectar si los cambias de estado
 int botonViejo1 = 0;
 int botonViejo2 = 0;
 int botonViejo3 = 0;
 
-int money = 0;
+int dinero = 0;
+
 // este es el mensaje para Twitter, cambialo por lo que quieras
 String mensaje = "Nada no pasa nada!";
 
 void setup() {
+  
   // configura el pin del LED
   pinMode(O0, OUTPUT); // 1 euro
   pinMode(O1, OUTPUT); // 2 euro
@@ -41,30 +70,39 @@ void setup() {
 }
 
 void loop() {
+
+  // Lee el estado de los botones
   boton1 = digitalRead(I0);
   boton2 = digitalRead(I1);
   boton3 = digitalRead(I2);
 
+  // Comprueba si el botón se ha pulsado pero no apretado continuamente
   if ( boton1 == HIGH && botonViejo1 == LOW) {
-    money += COIN_VALUE_1;
+    // Suma el dinero en la hucha
+    dinero += VALOR_ROJO;
     Serial.print("+");
-    Serial.println(COIN_VALUE_1);
+    Serial.println(VALOR_ROJO);
+    // Enciende el LED rojo
     digitalWrite(O0, HIGH);
-    sendAmount();
+    enviaCantidad();
   }
   if ( boton2 == HIGH && botonViejo2 == LOW) {
-    money += COIN_VALUE_2;
+    // Suma el dinero en la hucha
+    dinero += VALOR_AMARILLO;
     Serial.print("+");
-    Serial.println(COIN_VALUE_2);
+    Serial.println(VALOR_AMARILLO);
+    // Enciende el LED amarillo
     digitalWrite(O1, HIGH);
-    sendAmount();
+    enviaCantidad();
   }
   if ( boton3 == HIGH && botonViejo3 == LOW) {
-    money += COIN_VALUE_3;
+    // Suma el dinero en la hucha
+    dinero += VALOR_VERDE;
     Serial.print("+");
-    Serial.println(COIN_VALUE_3);
+    Serial.println(VALOR_VERDE);
+    // Enciende el LED verde
     digitalWrite(O2, HIGH);
-    sendAmount();
+    enviaCantidad();
   }
 
   // reinicia las variables del boton
@@ -72,29 +110,32 @@ void loop() {
   botonViejo2 = boton2;
   botonViejo3 = boton3;
 
-  Serial.print("My hucha : ");
-  Serial.println(money);
+  Serial.print("I hucha : ");
+  Serial.println(dinero);
 
   delay(50);
+
+  // Apaga los LEDs
   digitalWrite(O0, LOW);
   digitalWrite(O1, LOW);
   digitalWrite(O2, LOW);
-
 }
 
 
-void sendAmount() {
+void enviaCantidad() {
 
   // envia un tuit, majete
+  // Tweet con la cantidad total de dinero que tu tienes
+
   String servidor = "http://verkstad.cc/iot/zgz/code/twitter/code/sendTweet.php?pass=tobbe&message=";
   String url = "";
-  mensaje = mensaje + money;
+  mensaje = mensaje + dinero;
   mensaje = urlEncode(mensaje); // esto ajusta el mensaje para ponerlo en la url
   url = servidor + mensaje;
   client.get(url);
 
   Serial.print("Tuit enviado! " );
-  Serial.println(money);
+  Serial.println(dinero);
 
   delay(500);
 }
